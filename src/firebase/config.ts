66d59341010ae5dfae21730ffdb21a98d13721b0
Jonathan Ndakola <j.ndakola1@gmail.com>
2026-03-03 20:01:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "gemini-switch-2fdeg",
+  "appId": "1:516197541019:web:a1f0ad5e0e31085b4a123e",
+  "apiKey": "AIzaSyB5NaAMWWbPKqvqLU3s6oaJLi58sIXpqK4",
+  "authDomain": "gemini-switch-2fdeg.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "516197541019"
+};

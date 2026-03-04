7539255a332af6ce46aca05081f6c6a9705033e9
Jonathan Ndakola <j.ndakola1@gmail.com>
2026-03-04 21:47:22 +0000

    So now we can connect our auth

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
+
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "REPLACE_ME",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-7352806100-143c2.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-7352806100-143c2",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "studio-7352806100-143c2.firebasestorage.app",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "950293011656",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:950293011656:web:12122997a33ed43ffa59fd"
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "gemini-switch-2fdeg.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "gemini-switch-2fdeg",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "gemini-switch-2fdeg.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "516197541019",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:516197541019:web:a1f0ad5e0e31085b4a123e"
 };
